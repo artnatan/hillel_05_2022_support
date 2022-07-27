937d8898017af1008d8f22a9e3f53f6fd811ce60
artnatan <artnatan74@gmail.com>
2022-07-27 18:53:15 +0300

    refactoring

---
+++ config/api/tickets.py
@@ -0,0 +1,12 @@
+from django.http import JsonResponse
+
+
+class TicketService:
+    def get_all_tickets(self) -> dict:
+        return {}
+
+def get_all_tickets(request) -> dict:
+    tickets_service = TicketService()
+    data = tickets_service.get_all_tickets()
+    
+    return JsonResponse(data)
