50b9e0d1d68730fc15651aa171eb3b680b19ae94
artnatan <artnatan74@gmail.com>
2022-09-14 17:06:21 +0300

    validation password

--- config/validators.py
+++ config/validators.py
@@ -1,12 +1,11 @@
-# from django.core.exceptions import ValidationError 
+# from django.core.exceptions import ValidationError
 
 from django.contrib.auth import get_user_model
-
 
 User = get_user_model()
 
 
 class MyCustomPasswordValidator:
-    def validate(self, password: str, user: User | None = None):    #type: ignore
+    def validate(self, password: str, user: User | None = None):  # type: ignore
         # raise ValidationError("My custom Error")
         return
